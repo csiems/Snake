f2934fb18a46cf37196a5886e7f215a7026d0641
Christopher Siems <csiems@gmail.com>
2020-07-09 19:33:35 -0700

    add interface and sample files

--- src/main/java/com/vevo/hiring/snake/Main.java
+++ src/main/java/com/vevo/hiring/snake/Main.java
@@ -7,21 +7,21 @@ public class Main {
         SnakeGame game = new MySnakeGame(10, 10);
         System.out.println(game.getGameBoard());
 
-        System.out.println("----> MOVE EAST (true)");
-        game.move(SnakeGame.Direction.EAST, true);
-        System.out.println(game.getGameBoard());
-
-        System.out.println("----> MOVE EAST (false)");
-        game.move(SnakeGame.Direction.EAST, false);
-        System.out.println(game.getGameBoard());
-
-        System.out.println("----> MOVE EAST (false)");
-        game.move(SnakeGame.Direction.EAST, false);
-        System.out.println(game.getGameBoard());
-
-        System.out.println("\\/\\/  MOVE SOUTH (true)");
-        game.move(SnakeGame.Direction.SOUTH, true);
-        System.out.println(game.getGameBoard());
+//        System.out.println("----> MOVE EAST (true)");
+//        game.move(SnakeGame.Direction.EAST, true);
+//        System.out.println(game.getGameBoard());
+//
+//        System.out.println("----> MOVE EAST (false)");
+//        game.move(SnakeGame.Direction.EAST, false);
+//        System.out.println(game.getGameBoard());
+//
+//        System.out.println("----> MOVE EAST (false)");
+//        game.move(SnakeGame.Direction.EAST, false);
+//        System.out.println(game.getGameBoard());
+//
+//        System.out.println("\\/\\/  MOVE SOUTH (true)");
+//        game.move(SnakeGame.Direction.SOUTH, true);
+//        System.out.println(game.getGameBoard());
 
     }
 
--- src/main/java/com/vevo/hiring/snake/MySnakeGame.java
+++ src/main/java/com/vevo/hiring/snake/MySnakeGame.java
@@ -2,11 +2,20 @@ package com.vevo.hiring.snake;
 
 public class MySnakeGame implements SnakeGame {
 
+    Board myBoard;
+
     public MySnakeGame(int numberOfRows, int numberOfColumns) {
+        myBoard = new Board(numberOfRows, numberOfColumns);
     }
 
     public String getGameBoard() {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        for (char[] row : myBoard.getRows()) {
+            sb.append(row);
+            sb.append('\n');
+        }
+        return sb.toString();
     }
 
     public void move(Direction direction, boolean grow) {
