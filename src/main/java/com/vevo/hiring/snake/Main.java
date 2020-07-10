f2dcb995ea8b8d8babf14c54f7cb309b9c303b5e
Christopher Siems <csiems@gmail.com>
2020-07-09 18:31:55 -0700

    initial commit

---
+++ src/main/java/com/vevo/hiring/snake/Main.java
@@ -0,0 +1,28 @@
+package com.vevo.hiring.snake;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SnakeGame game = new MySnakeGame(10, 10);
+        System.out.println(game.getGameBoard());
+
+        System.out.println("----> MOVE EAST (true)");
+        game.move(SnakeGame.Direction.EAST, true);
+        System.out.println(game.getGameBoard());
+
+        System.out.println("----> MOVE EAST (false)");
+        game.move(SnakeGame.Direction.EAST, false);
+        System.out.println(game.getGameBoard());
+
+        System.out.println("----> MOVE EAST (false)");
+        game.move(SnakeGame.Direction.EAST, false);
+        System.out.println(game.getGameBoard());
+
+        System.out.println("\\/\\/  MOVE SOUTH (true)");
+        game.move(SnakeGame.Direction.SOUTH, true);
+        System.out.println(game.getGameBoard());
+
+    }
+
+}
---
+++ src/main/java/com/vevo/hiring/snake/MySnakeGame.java
@@ -0,0 +1,15 @@
+package com.vevo.hiring.snake;
+
+public class MySnakeGame implements SnakeGame {
+
+    public MySnakeGame(int numberOfRows, int numberOfColumns) {
+    }
+
+    public String getGameBoard() {
+        return null;
+    }
+
+    public void move(Direction direction, boolean grow) {
+
+    }
+}
